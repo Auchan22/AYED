13d03a2f57727c2a92557f7ba5e9d50cc713fea5
Auchan22 <agustinsurila@gmail.com>
2024-06-09 19:19:49 -0300

    Inciso 3 y 4 ejercicio 3

--- src/tp5/ej3/Mapa.java
+++ src/tp5/ej3/Mapa.java
@@ -54,4 +54,108 @@ public class Mapa {
         marcas[o.getPosition()] = false; //Deja desmarcadas todas las posiciones del vector de marcas: no es necesario desmarcar en este ejemplo.
         return encontre;
     }
+    /*----------------------- Inciso 2 -----------------------*/
+    public List<String> devolverCaminoExceptuando(String c1, String c2, List<String> ciudades){
+        List<String> camino = new LinkedList<String>();
+
+        if(!this.mapaCiudades.isEmpty()){
+            Vertex<String> origen = this.mapaCiudades.search(c1);
+            Vertex<String> destino = this.mapaCiudades.search(c2);
+            if(origen != null && destino != null){
+                boolean[] marcas = new boolean[this.mapaCiudades.getSize()];
+                this.marcarRestringidos(ciudades, marcas);
+                this.devolverCamino(origen, destino, camino, marcas);
+            }
+        }
+
+        return camino;
+    }
+
+    private void marcarRestringidos(List<String> c, boolean[] m){
+        for (String n: c){
+            Vertex<String> v = this.mapaCiudades.search(n);
+            if(v!=null){
+                //Ya lo marco, por lo que si en el recorrido lo encuentra visitado, no pasara por ahi
+                m[v.getPosition()]=true;
+            }
+        }
+    }
+    /*----------------------- Inciso 3 -----------------------*/
+    public List<String> caminoMasCorto(String c1, String c2){
+        List<String> min = new LinkedList<>();
+
+        if(!this.mapaCiudades.isEmpty()){
+            Vertex<String> origen = this.mapaCiudades.search(c1);
+            Vertex<String> destino = this.mapaCiudades.search(c2);
+            if(origen != null && destino != null){
+                boolean[] marcas = new boolean[this.mapaCiudades.getSize()];
+                this.caminoMasCorto(origen, destino, min, new LinkedList<String>(), marcas, 0, Integer.MAX_VALUE);
+            }
+        }
+
+        return min;
+    }
+
+    private int caminoMasCorto(Vertex<String> o, Vertex<String> d, List<String> caminoMin, List<String> caminoAct, boolean[] marcas, int total, int min){
+        marcas[o.getPosition()] = true;
+        caminoAct.add(o.getData());
+        if(o == d && total < min){
+            caminoMin.removeAll(caminoMin);
+            caminoMin.addAll(caminoAct);
+            min = total;
+        }else{
+            List<Edge<String>> adyacencias = this.mapaCiudades.getEdges(o);
+            Iterator<Edge<String>> it = adyacencias.iterator();
+            while(it.hasNext() && total < min){
+                Edge<String> v = it.next();
+                int pos = v.target().getPosition();
+                int aux = total + v.getWeight();
+                if(!marcas[pos] && aux < min){
+                    min = caminoMasCorto(v.target(), d, caminoMin, caminoAct, marcas, aux, min);
+                }
+            }
+        }
+        marcas[o.getPosition()] = false;
+        caminoAct.remove(caminoAct.size()-1);
+        return min;
+    }
+    /*----------------------- Inciso 4 -----------------------*/
+    public List<String> caminoSinCargarCombustible(String c1, String c2, int tanqueAuto){
+        List<String> camino = new LinkedList<String>();
+
+        if(!this.mapaCiudades.isEmpty()){
+            Vertex<String> origen = this.mapaCiudades.search(c1);
+            Vertex<String> destino = this.mapaCiudades.search(c2);
+            if(origen != null && destino != null){
+                boolean[] marcas = new boolean[this.mapaCiudades.getSize()];
+                this.caminoSinCargarCombustible(origen, destino, camino, marcas, tanqueAuto);
+            }
+        }
+
+        return camino;
+    }
+
+    private boolean caminoSinCargarCombustible(Vertex<String> o, Vertex<String> d, List<String> camino, boolean[] marcas, int tanque){
+        boolean encontre = false;
+        marcas[o.getPosition()] = true;
+        camino.add(o.getData());
+        if(o == d){
+            //Llegamos al final
+            return true;
+        }else{
+            List<Edge<String>> adyacencias = this.mapaCiudades.getEdges(o);
+            Iterator<Edge<String>> it = adyacencias.iterator();
+            while (it.hasNext() && !encontre){
+                Edge<String> e = it.next();
+                int pos = e.target().getPosition();
+                if(!marcas[pos] && tanque-e.getWeight() > 0){
+                    encontre = caminoSinCargarCombustible(e.target(), d, camino, marcas, tanque-e.getWeight());
+                }
+            }
+        }
+        marcas[o.getPosition()] = false;
+        camino.remove(o.getData());
+        return encontre;
+    }
+    /*----------------------- Inciso 5 -----------------------*/
 }
